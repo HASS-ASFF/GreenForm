ae4cc884a93d86e78bcb47af5ad1aac07ab79859
HASS-ASFF <hardesk123@gmail.com>
2021-08-11 01:36:00 +0100

    Remplissage modeles

--- Projet Green Form/greenform_project/greenform_app/admin.py
+++ Projet Green Form/greenform_project/greenform_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Membre)
+admin.site.register(Centre_formation)
+admin.site.register(Personne)
+admin.site.register(Inscription)
+admin.site.register(Abonnement)
+admin.site.register(Adherent)
+admin.site.register(Etablissement)
+admin.site.register(Partenaire)
+admin.site.register(Activite)
